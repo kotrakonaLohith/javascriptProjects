c1aeaa007dd194a204061cf05bb9ff8364a11021
Lohith Kotrakona <Lohith.Kotrakona@rotary.org>
2020-01-29 15:13:04 -0600

    wrote comments on functions and linted files

--- form/script.js
+++ form/script.js
@@ -3,6 +3,8 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
+
+// This function validates the form and returns error if it fails
 function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = "form-control error";
@@ -11,11 +13,13 @@ function showError(input, message) {
   small.innerText = message;
 }
 
+// This function validates the form and returns success if all the conditions are met.
 function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = "form-control success";
 }
 
+// This function validates the email entered with the regex and throws success or error messages
 function checkEmail(input) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value.trim())) {
@@ -25,6 +29,7 @@ function checkEmail(input) {
   }
 }
 
+// This function makes the input field required if not entered
 function checkRequired(inputArr) {
   inputArr.forEach(function(input) {
     if (input.value.trim() === "") {
@@ -35,6 +40,8 @@ function checkRequired(inputArr) {
   });
 }
 
+//This function checks for the length of the characters entered in the
+//input field and returns success if satisfied else throws an error
 function checkLength(input, min, max) {
   if (input.value.length < min) {
     showError(
@@ -51,44 +58,20 @@ function checkLength(input, min, max) {
   }
 }
 
+// This function validates that the passwords match or not
 function checkPasswordsMatch(input1, input2) {
   if (input1.value !== input2.value) {
     showError(input2, "Passwords didn't match");
   }
 }
 
+// Error message to uppercase
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 form.addEventListener("submit", function(e) {
   e.preventDefault();
-
-  //   if (username.value === "") {
-  //     showError(username, "Username is required");
-  //   } else {
-  //     showSuccess(username);
-  //   }
-
-  //   if (email.value === "") {
-  //     showError(email, "email is required");
-  //   } else if (!isValidEmail(email.value)) {
-  //     showError(email, "email is not valid");
-  //   } else {
-  //     showSuccess(email);
-  //   }
-
-  //   if (password.value === "") {
-  //     showError(password, "password is required");
-  //   } else {
-  //     showSuccess(password);
-  //   }
-
-  //   if (password2.value === "") {
-  //     showError(password2, "password 2 is required");
-  //   } else {
-  //     showSuccess(password2);
-  //   }
   checkRequired([username, email, password, password2]);
   checkLength(username, 3, 15);
   checkLength(password, 6, 25);
