a5135d78c27992a4c0ac623fae700ac7f88f60a7
Lohith Kotrakona <Lohith.Kotrakona@rotary.org>
2020-01-29 14:27:55 -0600

    enabled form validation to the input fields

--- form/script.js
+++ form/script.js
@@ -16,19 +16,49 @@ function showSuccess(input) {
   formControl.className = "form-control success";
 }
 
-function isValidEmail(email) {
+function checkEmail(input) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
 }
 
 function checkRequired(inputArr) {
   inputArr.forEach(function(input) {
     if (input.value.trim() === "") {
-      showError(input, "is required");
+      showError(input, `${getFieldName(input)} is required`);
     } else {
       showSuccess(input);
     }
   });
+}
+
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be atleast ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords didn't match");
+  }
+}
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 form.addEventListener("submit", function(e) {
@@ -60,4 +90,8 @@ form.addEventListener("submit", function(e) {
   //     showSuccess(password2);
   //   }
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
