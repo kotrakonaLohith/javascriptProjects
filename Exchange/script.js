35cf7b2b9ef09043e1a127ec1f52436049a8dcbf
Lohith Kotrakona <Lohith.Kotrakona@rotary.org>
2020-02-11 12:12:14 -0600

    deleted sample json file and added logic to the calculator to function

--- Exchange/script.js
+++ Exchange/script.js
@@ -1,3 +1,38 @@
-function calculate() {}
+const currencyElement_one = document.getElementById("currency-one");
+const amountElement_one = document.getElementById("amount-one");
+const currencyElement_two = document.getElementById("currency-two");
+const amountElement_two = document.getElementById("amount-two");
+
+const rateElement = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+//fetching the exchange rates and updating the DOM
+function calculate() {
+  const currency_one = currencyElement_one.value;
+  const currency_two = currencyElement_two.value;
+  //console.log(currency_one, currency_two);
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(data => {
+      const rate = data.rates[currency_two];
+      // console.log(rate);
+      rateElement.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountElement_two.value = (amountElement_one.value * rate).toFixed(2);
+    });
+}
+
+currencyElement_one.addEventListener("change", calculate);
+amountElement_one.addEventListener("input", calculate);
+currencyElement_two.addEventListener("change", calculate);
+amountElement_two.addEventListener("input", calculate);
+
+// added event listener to swap the curreny elements
+swap.addEventListener("click", () => {
+  const temp = currencyElement_one.value;
+  currencyElement_one.value = currencyElement_two.value;
+  currencyElement_two.value = temp;
+  calculate();
+});
 
 calculate();
