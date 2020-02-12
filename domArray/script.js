004aac2c0b4d87455442d66fbcef20c301ca555e
Lohith Kotrakona <Lohith.Kotrakona@rotary.org>
2020-02-12 11:11:42 -0600

    test push to test buddy

---
+++ domArray/script.js
@@ -0,0 +1,6 @@
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const addUserBtn = document.getElementById("double");
+const addUserBtn = document.getElementById("show-millionaires");
+const addUserBtn = document.getElementById("sort");
+const addUserBtn = document.getElementById("calculate-wealth");
