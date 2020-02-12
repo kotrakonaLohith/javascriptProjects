b4a751052f689daf070098cc5f63dbed3d5bc2b9
Lohith Kotrakona <Lohith.Kotrakona@rotary.org>
2020-02-12 12:19:36 -0600

    added logic to add user button that generates a random user name and amounts

--- domArray/script.js
+++ domArray/script.js
@@ -1,6 +1,48 @@
 const main = document.getElementById("main");
 const addUserBtn = document.getElementById("add-user");
-const addUserBtn = document.getElementById("double");
-const addUserBtn = document.getElementById("show-millionaires");
-const addUserBtn = document.getElementById("sort");
-const addUserBtn = document.getElementById("calculate-wealth");
+const doubleBtn = document.getElementById("double");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+//fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+  const user = data.results[0];
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  };
+  //   console.log(newUser);
+  addData(newUser);
+}
+
+function addData(user) {
+  data.push(user);
+
+  updateDom();
+}
+
+function updateDom(providedData = data) {
+  main.innerHTML = "<h2><strong>Person</strong>Wealth</h2>";
+  providedData.forEach(item => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+//Used Intl API to convert the number to currency
+function formatMoney(amount) {
+  return new Intl.NumberFormat("en-EN", {
+    style: "currency",
+    currency: "USD"
+  }).format(amount);
+}
+
+addUserBtn.addEventListener("click", getRandomUser);
